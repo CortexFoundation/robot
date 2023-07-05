a6d031c08aadbf61ecf68d0d59bdd1c14eac674e
ucwong <ucwong@126.com>
2023-07-05 21:00:47 +0800

    log & tiny fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/CortexFoundation/CortexTheseus/log"
 	"github.com/CortexFoundation/robot"
 	"github.com/CortexFoundation/torrentfs/params"
 )
 
 func main() {
+	glogger := log.NewGlogHandler(log.StreamHandler(os.Stderr, log.TerminalFormat(true)))
+	glogger.Verbosity(log.LvlInfo)
+	glogger.Vmodule("")
+	log.Root().SetHandler(glogger)
 	cfg := &params.DefaultConfig
-	cfg.DataDir = "test"
+	cfg.DataDir = ".storage"
+	cfg.RpcURI = "http://127.0.0.1:8545"
 	if m, err := robot.New(cfg, true, false, false, nil); err != nil {
 		panic(err)
 	} else {
-		m.Start()
-		m.Stop()
+		if err := m.Start(); err != nil {
+			log.Error("start failed", "err", err)
+		}
+		defer m.Stop()
+		var c = make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		<-c
 	}
 }
