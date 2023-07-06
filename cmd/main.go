6b68681b64e727a91e84faff934d65cb391f9a91
ucwong <ucwong@126.com>
2023-07-06 21:53:03 +0800

    monitor start fix

--- cmd/main.go
+++ cmd/main.go
@@ -15,14 +15,17 @@ func main() {
 	glogger.Verbosity(log.LvlInfo)
 	glogger.Vmodule("")
 	log.Root().SetHandler(glogger)
+
 	cfg := &params.DefaultConfig
 	cfg.DataDir = ".storage"
 	cfg.RpcURI = "http://127.0.0.1:8545"
+
 	if m, err := robot.New(cfg, true, false, false, nil); err != nil {
 		panic(err)
 	} else {
 		if err := m.Start(); err != nil {
 			log.Error("start failed", "err", err)
+			panic(err)
 		}
 		defer m.Stop()
 		var c = make(chan os.Signal, 1)
